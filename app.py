6f90a0c15f2a8b561a711bd884e81a5c9042b027
ryosuke1217 <ryosuke121720080711@gmail.com>
2018-01-11 19:06:40 +0900

    schema作成

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
 from flask import Flask
+import json
+from jsonschema import validate
 app = Flask(__name__)
+
+schema = {
+    'type': 'object',
+    'properties': {
+        'items': {
+            'item': {
+                'type': 'string'
+            },
+            'sequence': {
+                'type': 'int'
+            }
+            'type': 'array',
+        }
+    },
+    'required': [
+        'items',
+        'item',
+        'name'
+    ]
+}
 
 @app.route("/")
 def hello():
