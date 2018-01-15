bedad6e58630c790c577605a23858547b1b0c1d2
ryosuke1217 <ryosuke121720080711@gmail.com>
2018-01-15 14:30:08 +0900

    cerberusでのバリデーションチェック追加

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, abort
 from jsonschema import validate, ValidationError
 import json
 import validate as vl
@@ -6,16 +6,24 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def index():
+    if not request.method == 'POST':
+        abort(405)
     data = json.loads(request.data.decode('utf-8'))
     ret = vl.valid(data)
-    return ret
-    # print(data)
-    # # print(schema)
-    # try:
-    #     validate(data, schema)
-    # except ValidationError as e:
-    #     print('Invalid JSON - {0}'.format(e.message))
-    # return data["items"][0]["item"]
+    if not ret == 'OK':
+        abort(400)
+    return jsonify({"message": "OK"})
+
+@app.errorhandler(400)
+def error_handler(error):
+    print(error)
+    response = jsonify({ 'message': "invalid parameter", 'result': error.code })
+    return response, error.code
+
+@app.errorhandler(405)
+def error_handler(error):
+    response = jsonify({ 'message': "invalid request method", 'result': error.code })
+    return response, error.code
 
 if __name__ == "__main__":
     app.run(host='192.168.33.10', port=5000)
--- validate.py
+++ validate.py
@@ -1,62 +1,121 @@
 
 from jsonschema import validate, ValidationError
+import cerberus
 import json
 
-# schema = {
-#     "type": "object",
-#     "properties": {
-#         "items": {
-#             "type": "array",
-#             "items": {
-#                 "type": "object",
-#                 "properties": {
-#                     "item": {
-#                         "type": "string",
-#                         "minLength": 1
-#                     },
-#                     "sequence": {
-#                         "type": "int",
-#                         "minimum": 0,
-#                         "maximum": 999
-#                     }
-#                 },
-#                 "required": [
-#                     "item",
-#                     "sequence"
-#                 ]
-#             }
-#         },
-#         "required": ["items"]
-#     }
-# }
+input_schema = {
+    "items": {
+        "required": True,
+        "type": "list",
+        "schema": {
+            "type": "dict",
+            "schema": {
+                "item": {
+                    "required": True,
+                    "type": "string",
+                    "minlength": 1,
+                    "empty": False
+                },
+                "sequence": {
+                    "required": True,
+                    "type": "integer",
+                    "min": 0,
+                    "max": 999,
+                    "empty": False
+                }
+            }
+        }
+    }
+}
+
+output_schema = {
+    "results": {
+        "required": True,
+        "type": "list",
+        "schema": {
+            "type": "dict",
+            "schema": {
+                "sequence": {
+                    "required": True,
+                    "type": "integer",
+                    "min": 0,
+                    "max": 9999999999,
+                    "empty": False
+                 },
+                "status": {
+                    "required": True,
+                    "type": "integer",
+                    "min": 0,
+                    "max": 1,
+                    "empty": False
+                },
+                "input_cnt": {
+                    "type": "integer",
+                    "min": 0,
+                    "max": 999
+                },
+                "unk_cnt": {
+                    "type": "integer",
+                    "min": 0,
+                    "max": 999
+                },
+                "rec_top10": {
+                    "type": "list",
+                    "schema": {
+                        "type": "dict",
+                        "schema": {
+                            "cat_id": {
+                                "type": "integer",
+                                "min": 0
+                            },
+                            "likelihood": {
+                                "type": "float",
+                                "min": 0,
+                                "max": 9999999999
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 def valid(data):
-    if "items" not in data.keys():
-        return "キーが不正です"
-    return checkObj(data["items"])
-
-def checkObj(data):
-    if not isinstance(data, list):
-        return "itemの値が配列ではありません。"
-    
-    for obj in data:
-        print(obj)
-        if not obj.keys() >= {"item", "sequence"}:
-            return "キーが不正です。"
-        if not checkItem(obj["item"]) or not checkSequence(obj["sequence"]):
-            return "値が不正です。"
+    v = cerberus.Validator(input_schema)
+    print(v.validate(data))
+    print(v.errors)
+    if not v.validate(data):
+        return json.dumps(v.errors)
     return "OK"
 
-def checkItem(val):
-    if not isinstance(val, str):
-        return False
-    if not len(val) >= 1:
-        return False
-    return True
+# def valid(data):
+#     if "items" not in data.keys():
+#         return "itemsが存在しません"
+#     return checkObj(data["items"])
 
-def checkSequence(val):
-    if not isinstance(val, int):
-        return False
-    if not not val >= 0 and not val <= 999:
-        return False
-    return True
+# def checkObj(data):
+#     if not isinstance(data, list):
+#         return "itemsの値が配列ではありません。"
+    
+#     for obj in data:
+#         print(obj)
+#         if not obj.keys() >= {"item", "sequence"}:
+#             return "キーが不正です。"
+#         if not checkItem(obj["item"]) or not checkSequence(obj["sequence"]):
+#             return "値が不正です。"
+#     return "OK"
+
+# def checkItem(val):
+#     if not isinstance(val, str):
+#         return False
+#     if not len(val) >= 1:
+#         return False
+#     return True
+
+# def checkSequence(val):
+#     if not isinstance(val, int):
+#         return False
+#     if not not val >= 0 and not val <= 999:
+#         return False
+#     return True
